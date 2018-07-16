8e2bfca2c659f1d5124e47928b557d4256c09bbb
Syed Abuthahir <abuvanth@gmail.com>
2018-07-16 17:49:48 +0530

    Update html2dj.py

--- html2dj.py
+++ html2dj.py
@@ -7,15 +7,15 @@ for ft in files:
     soup=BeautifulSoup(htm,'html.parser')
     for i in soup.find_all('script'):
         if i.has_attr('src'):
-           if str(i['src']).startswith('assets'):
+           if str(i['src']).startswith('assets'): # assets should be changed to js if the root folder of js file is 'js'
               i['src']="{% static '"+i['src']+"' %}"
     for j in soup.find_all('link'):
         if j.has_attr('href'):
-           if str(j['href']).startswith('assets'):
+           if str(j['href']).startswith('assets'): # assets should be changed to css if the root folder of css file is 'js'
               j['href']="{% static '"+j['href']+"' %}"
     for k in soup.find_all('img'):
         if k.has_attr('src'):
-           if str(k['src']).startswith('assets'):
+           if str(k['src']).startswith('assets'): # assets should be changed to images if the root folder of image file is 'images'
               k['src']="{% static '"+k['src']+"' %}"
     with open('dj-'+ft.split('/')[-1],'w') as fil:
          fil.write('{% load staticfiles %}\n'+str(soup))
